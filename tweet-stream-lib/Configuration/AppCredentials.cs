0c36fabd9a524de5ab66393b3b0eee7a1f9dddca
weirdyang <wy4786@gmail.com>
2022-08-27 13:49:17 +0800

    remove app credentials

--- tweet-stream-lib/Configuration/AppCredentials.cs
+++ tweet-stream-lib/Configuration/AppCredentials.cs
@@ -5,9 +5,7 @@ using System.Text;
 namespace TweetStream.Core.Configuration
 {
     public class AppCredentials
-    {   // api key a9IL6cV20YPrZXSVD4o8VCMH0
-        // api secret z9TwmGmop0EJuk4KezHD5WyENLENrnCe93mYqO6RY0VbUeQ6QB
-        // bearer token AAAAAAAAAAAAAAAAAAAAAPn3XwEAAAAAQNGxCf6sZS8ONnihanboKZR7Bg8%3DiNXWz8s3LoKYFF2UvetBpPYd1zxrUqB0GYugaiKAt828prEr2j
+    {  
         public string ConsumerKey { get; set; }
         public string ConsumerSecret { get; set; }
         public string BearerToken { get; set; }
